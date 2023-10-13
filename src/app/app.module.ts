5cd14bf3b7bd533e831115d6a16b03cfdad71de6
Dash <Promise.Dash@chubb.com>
2023-10-13 21:43:25 +0530

    small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    NavComponent
   ],
   imports: [
     BrowserModule,
